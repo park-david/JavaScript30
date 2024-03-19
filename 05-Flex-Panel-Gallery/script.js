4f7cc7ac055de32a6648102c326e2c55e1440f4d
park-david <davidpark1094@gmail.com>
2024-03-18 20:56:56 -0700

    finished flex panel gallery

---
+++ 05-Flex-Panel-Gallery/script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel')
+
+// todo only allow 1 active panel at a time
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName)
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))

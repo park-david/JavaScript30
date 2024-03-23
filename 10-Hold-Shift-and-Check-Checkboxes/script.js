82a4ee7d7f671b4cc3e1416cc6f9b09a1ee2e131
park-david <davidpark1094@gmail.com>
2024-03-23 16:33:19 -0700

    finished hold shift to check multiple checkboxes

---
+++ 10-Hold-Shift-and-Check-Checkboxes/script.js
@@ -0,0 +1,23 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+let lastChecked;
+
+function handleCheck(e) {
+    // check if shift is pressed
+    // check that they are checking it
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))

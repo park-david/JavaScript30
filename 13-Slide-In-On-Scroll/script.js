980027549bea2cfb641a364ed67cd9d963274ff1
park-david <davidpark1094@gmail.com>
2024-03-27 17:41:38 -0700

    finished slide in on scroll

---
+++ 13-Slide-In-On-Scroll/script.js
@@ -0,0 +1,33 @@
+// adds a delay so scroll doesnt get logged too many times
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+        const sliderInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = sliderInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active')
+        } else {
+            sliderImage.classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
